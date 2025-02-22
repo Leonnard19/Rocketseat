6c09117f4dce36b7767f76e2f3f5a055bc3c719a
Leonardo <srleon19@hotmail.com>
2025-02-21 22:29:51 -0300

    conceitos react

---
+++ Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post";
+
+function App() {
+  return (
+    <>
+      <Post author="leonardo" content="Lorem ipsum bla bla bla" />
+      <Post author="Gabriel" content="Lorem ipsum tchu tchu tchu" />
+    </>
+  );
+}
+
+export default App;
---
+++ Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,6 @@
+export const Post = (props) => {
+  <>
+    <strong>{props.author}</strong>
+    <h1>{props.content}</h1>
+  </>;
+};
