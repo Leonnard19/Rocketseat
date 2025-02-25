fd2368d940d058f73f489345a596b22615c05dee
Leonardo <srleon19@hotmail.com>
2025-02-25 18:21:02 -0300

    finish sidebar

--- Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +10,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+      <img className={styles.avatar} src="https://github.com/leonnard19.png" alt="user_logo" />
+
         <strong>Leonardo Santos</strong>
-        <span>Full-stack Developer</span>
+        <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
