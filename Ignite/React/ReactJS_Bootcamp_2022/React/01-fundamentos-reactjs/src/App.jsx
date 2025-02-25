c71c4f7358529951010568d03b3b4133f96e37eb
Leonardo <srleon19@hotmail.com>
2025-02-25 18:00:22 -0300

    Component: Sidebar

--- Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/App.jsx
+++ Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/App.jsx
@@ -1,19 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
 import "./global.css";
+
 
 function App() {
   return (
     <>
       <Header />
-      <Post
-        author="leonardo"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
-      <Post
-        author="Gabriel"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="leonardo"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+        </main>
+      </div>
     </>
   );
 }
