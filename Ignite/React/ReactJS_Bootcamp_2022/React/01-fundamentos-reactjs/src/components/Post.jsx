df82236cdcc005f824f0b06c0ee2476f17194fb7
Leonardo <srleon19@hotmail.com>
2025-02-26 18:32:30 -0300

    Post props

--- Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/components/Post.jsx
+++ Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,33 +1,49 @@
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
-import styles from "./Post.module.css";
+import { format, formatDistanceToNow } from 'date-fns';
 
-export function Post() {
+import styles from "./Post.module.css";
+import ptBR from 'date-fns/locale/pt-BR';
+
+export function Post({author, publishedAt, content}) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/leonnard19.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Leonardo Santos</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
-          Publicado há 1h
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="">jane.design/doctorcare</a>
-        </p>
+        {content.map((line, index) => {
+          if (line.type === "paragraph") {
+            return <p key={index}>{line.content}</p>;
+          }
+
+          if (line.type === "link") {
+            return (
+              <p key={index}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
         <p>
           <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
           <a href="">#rocketseat</a>
@@ -43,7 +59,6 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
         <Comment />
         <Comment />
       </div>
