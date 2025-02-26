84c4c962377abaa143875caeb46364383d3338f9
Leonardo <srleon19@hotmail.com>
2025-02-26 18:57:37 -0300

    add new comment feature

--- Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/components/Post.jsx
+++ Ignite/React/ReactJS_Bootcamp_2022/React/01-fundamentos-reactjs/src/components/Post.jsx
@@ -7,7 +7,8 @@ import ptBR from "date-fns/locale/pt-BR";
 import { useState } from "react";
 
 export function Post({ author, publishedAt, content }) {
-  const [comments, setComments] = useState([1, 2]);
+  const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
+  const [newCommentText, setNewCommentText] = useState("");
 
   const publishedDateFormatted = format(
     publishedAt,
@@ -24,7 +25,12 @@ export function Post({ author, publishedAt, content }) {
   function handleCrateNewComment(event) {
     event.preventDefault();
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
+  }
+
+  function handleNewCommentChange(event) {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -68,7 +74,12 @@ export function Post({ author, publishedAt, content }) {
 
       <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder="Deixe um comentário" />
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
         <footer>
           <button type="submit">Publicar</button>
         </footer>
@@ -76,7 +87,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment />;
+          return <Comment content={comment} />;
         })}
       </div>
     </article>
