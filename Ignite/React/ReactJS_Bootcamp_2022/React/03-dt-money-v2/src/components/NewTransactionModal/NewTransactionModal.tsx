aa42e61482fb0e432b5ff2dc75ffa28d60162d6f
Leonardo <srleon19@hotmail.com>
2025-03-05 16:35:21 -0300

    accessible radio button

--- Ignite/React/ReactJS_Bootcamp_2022/React/03-dt-money-v2/src/components/NewTransactionModal/NewTransactionModal.tsx
+++ Ignite/React/ReactJS_Bootcamp_2022/React/03-dt-money-v2/src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -17,17 +17,17 @@ export function NewTransactionModal() {
         <CloseButton>
           <X size={24} />
         </CloseButton>
-        
+
         <form>
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
           <TransactionType>
-            <TransactionTypeButton variant="income">
+            <TransactionTypeButton variant="income" value="income">
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
-            <TransactionTypeButton variant="outcome">
+            <TransactionTypeButton variant="outcome" value="outcome">
               <ArrowCircleDown size={24} />
               Saída
             </TransactionTypeButton>
