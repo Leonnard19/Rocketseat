0aed2a0deb558143fef39db1001583083765b269
Leonardo <srleon19@hotmail.com>
2025-03-14 17:24:02 -0300

    metrics loading skeleton

--- Ignite/React/ReactJS_Bootcamp_2022/React/pizzashop-web/src/lib/axios.ts
+++ Ignite/React/ReactJS_Bootcamp_2022/React/pizzashop-web/src/lib/axios.ts
@@ -8,12 +8,15 @@ export const api = axios.create({
   withCredentials: true,
 })
 
-// if (env.VITE_ENABLE_API_DELAY) {
-//   api.interceptors.request.use((config) => {
-//     return new Promise((resolve) => {
-//       setTimeout(() => {
-//         resolve(config)
-//       }, 2000)
-//     })
-//   })
-// }
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use((config) => {
+    return new Promise((resolve) => {
+      setTimeout(
+        () => {
+          resolve(config)
+        },
+        Math.round(Math.random() * 3000),
+      )
+    })
+  })
+}
