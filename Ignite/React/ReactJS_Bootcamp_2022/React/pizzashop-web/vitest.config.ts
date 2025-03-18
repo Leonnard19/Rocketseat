27d7f76fd2b389b49601758176f4d57f6cbebfb7
Leonardo <srleon19@hotmail.com>
2025-03-18 14:40:39 -0300

    pagination tests

--- Ignite/React/ReactJS_Bootcamp_2022/React/pizzashop-web/vitest.config.ts
+++ Ignite/React/ReactJS_Bootcamp_2022/React/pizzashop-web/vitest.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'node:path'
+
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -5,5 +7,9 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./test/setup.ts'],
     environment: 'happy-dom',
+  },
+  resolve: {
+    // vitest will resolve @ to the src folder
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
 })
